8fb05c644b719768fab9263a9ba5277b59f23b34
Ivan Seed <ivanseed@live.co.uk>
2017-09-14 23:26:18 +0100

    Switched to preact

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './style';
+import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export default App;
