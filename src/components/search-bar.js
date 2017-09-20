5e48027bc0fae436ac8f703447b0ec181b3f425b
Ivan Seed <ivanseed@live.co.uk>
2017-09-21 00:57:17 +0100

    Added sort selection buttons

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -24,6 +24,20 @@ export default class SearchBar extends Component {
             onChange={event => this.onInputChange(event.target.value)}
           />
         </div>
+        <form className="search-order-container">
+          <div className="search-order">
+            <input id="stars" type="radio" name="order" value="stars" />
+            <label className="order-stars" for="stars"></label>
+          </div>
+          <div className="search-order">
+            <input id="watchers" type="radio" name="order" value="watchers" />
+            <label className="order-watchers" for="watchers"></label>
+          </div>
+          <div className="search-order">
+            <input id="forks" type="radio" name="order" value="forks" />
+            <label className="order-forks" for="forks" alt="Forks"></label>
+          </div>
+        </form>
       </div>
     );
   }
