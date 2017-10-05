397d545a3af91f36307c65f5b97e4972e981f32b
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-10-05 20:41:38 +0200

    Fixed #41 Removed console.error on window resize

--- src/components/repository-list.js
+++ src/components/repository-list.js
@@ -3,9 +3,9 @@ import Repository from './repository';
 
 const repositoryList = ({items}) => (
   <div className="repositories">
-    {items.map((item, key) => {
-      return <Repository item={item} key={key} />
-    })}
+    {items.map((item, key) => (
+      <Repository item={item} key={key} />
+    ))}
   </div>
 );
 
