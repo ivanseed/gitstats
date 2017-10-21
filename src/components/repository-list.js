54a4f740faae582f477f63cfe6b7fa92309bf7de
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-10-21 17:28:49 -0400

    Don't show load more when loading first page

--- src/components/repository-list.js
+++ src/components/repository-list.js
@@ -8,7 +8,8 @@ const RepositoryList = ({ items, loadMore }) => (
     )}
     {
       // github api limits to 1000 items
-      items.length < 1000 &&
+      // don't show button when loading first page
+      0 < items.length && items.length < 1000 &&
       <div className="load-more" onClick={loadMore}>Load More</div>
     }
   </div>
